2fe5042aa7130745b7b9bbaf058cc05bea9f504b
SkPatel-12 <shubhamghodasara12@gmail.com>
2023-10-29 10:36:42 +0530

    Add sub routing error-page loading-page and components

--- Frontend/src/app/page.js
+++ Frontend/src/app/page.js
@@ -1,9 +1,17 @@
-import Image from 'next/image'
-
+import Footer from "./components/footer"
+import Header from "./components/header"
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Startup...</h1>
-    </main>
+    <>
+      <header>
+        <Header/>
+      </header>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Startup...</h1>
+      </main>
+      <footer>
+        <Footer/>
+      </footer>
+    </>
   )
 }
